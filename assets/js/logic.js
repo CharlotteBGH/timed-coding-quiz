03bacf7a7cf41e58da33f5971315f5f63539a530
Charlotte Brazier <charlottebrazier@hotmail.com>
2023-07-31 21:22:23 +0100

    created text for questions, started adding functions for starting the game and handling answers

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,24 +1,42 @@
-//document query selector
-var startBtn = document.getElementById("start");
-var timerSpan = document.getElementById("time");
+// List variables and add document query selectors
+
+// Time-related
+var startingTime = 60;
+var timer = document.getElementById("timer");
+var timeRemaining = document.getElementById("time-remaining");
+
+//Starting the quiz
 var startDiv = document.getElementById("start-screen");
+var startBtn = document.getElementById("start");
+
+//Answering the questions
 var questionsDiv = document.getElementById("questions");
+var answerChoices = document.getElementById("choices");
 
-// List variables
-var time = 60;
+//Submitting initials and scores
+var initials = document.getElementById("initials");
+var submitBtn = document.getElementById("submit");
+var seeHighScores = document.getElementById("scores");
 
-var buildQuestion = function () {
-  // target the question and each of the choices
-  // assignt the textValue to be the question/choices from your array
-};
-
-//handleanswer
-
+// Player starts game by clicking start button
 var startGame = function () {
-  timerSpan.innerText = time;
+  timer.innerText = timeRemaining;
   startDiv.classList.add("hide");
   questionsDiv.classList.remove("hide");
+  showQuestions();
+};
+
+// Player starts ansewering questions
+var showQuestions = function () {
+  //
+  // target the question and each of the choices
+  // assign the textValue to be the question/choices from your array
+};
+// What happens with the answer
+var handleAnswer = function () {
+  //
 };
 
 // event listeners
 startBtn.addEventListener("click", startGame);
+submitBtn.addEventListener("click", submit);
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,17 +1,17 @@
 var questionsArray = [
   {
-    questionText: "Question 1",
-    choices: ["choice1", "choice2", "choice3", "choice4"],
-    correctAnswer: "choice2",
+    questionText: "Which of these is NOT a programming language?",
+    choices: ["JavaScript", "Klingon", "Python", "C++"],
+    correctAnswer: "Klingon",
   },
   {
-    questionText: "Question 2",
-    choices: ["choice5", "choice6", "choice7", "choice8"],
-    correctAnswer: "choice33",
+    questionText: "Which operator invokes a function?",
+    choices: ["()", "¯_(ツ)_/¯", ":", "}"],
+    correctAnswer: "()",
   },
   {
-    questionText: "Question 3",
-    choices: ["choice9", "choice10", "choice11", "choice12"],
-    correctAnswer: "choice111",
+    questionText: "'If', 'for' and 'while' can be used to indicate a what?",
+    choices: ["array", "Web API", "h1 heading", "loop"],
+    correctAnswer: "loop",
   },
 ];
