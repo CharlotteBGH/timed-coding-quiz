4a9ea854f044999c1c01234c1e8b163c3af4040a
Charlotte Brazier <charlottebrazier@hotmail.com>
2023-08-03 12:56:32 +0100

    started adding function for what happens when users submit their answers

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,13 +1,12 @@
 // List variables and add document query selectors
 
-// Time-related
+// Time-related - add 60-second countdown clock
 var timeRemaining = 60;
 var clockTimer = document.getElementById("timer");
 var timerId = setInterval(countdown, 1000);
 function countdown() {
-  if (timeRemaining == 0) {
-    clearInterval(timerId);
-    //Startaskingquestions();
+  if (timeRemaining <= 0) {
+    endGame();
   } else {
     clockTimer.innerHTML = timeRemaining;
     timeRemaining--;
@@ -28,6 +27,10 @@ var initials = document.getElementById("initials");
 var submitBtn = document.getElementById("submit");
 var seeHighScores = document.getElementById("scores");
 
+//Ending the game
+var endScreen = document.getElementById("end-screen");
+var finalScore = document.getElementById("final-score");
+
 // Player starts game by clicking start button
 var startGame = function () {
   clockTimer.innerText = timeRemaining;
@@ -35,13 +38,6 @@ var startGame = function () {
   questionsDiv.classList.remove("hide"); // removes the "hide" class
   showQuestions();
 };
-
-// What is happening to the countdown clock
-setInterval(function () {
-  if (clockTimer > 0) clockTimer--;
-  document.getElementById("timer").innerHTML = timeRemaining;
-  if (clockTimer <= 0) clockTimer = 60;
-}, 1000);
 
 // Questions are displayed
 var showQuestions = function () {
@@ -56,12 +52,22 @@ var showQuestions = function () {
   // check if the text for the current chose answer matches the one in your currentQuestion
   // if yes, move onto following question else subtract time
 };
-// What happens with the answer
+// What happens with the answer - if text matches the one in
 var handleAnswer = function () {
-  //
+  if (this.value !== currentQuestion.correctAnswer) {
+    timeRemaining -= 10;
+    // if it's correct, move to the next question
+    // if it's incorrect, alert to say that the answer is wrong
+    // if time has run out, the game ends
+    if (timeRemaining <= 0) {
+      endGame();
+    }
+  }
 };
 
-var endGame = function () {};
+var endGame = function () {
+  clearInterval(timerId);
+};
 
 // event listeners
 startBtn.addEventListener("click", startGame);
