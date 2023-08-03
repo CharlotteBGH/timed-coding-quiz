d496fec5244f73050fb7065ec4bca87d043cc652
Charlotte Brazier <charlottebrazier@hotmail.com>
2023-08-04 00:17:58 +0100

    started adding event listeners for answer buttons, and timer to subtract 10 if the answer is wrong

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -7,6 +7,7 @@ var timerId = setInterval(countdown, 1000);
 function countdown() {
   if (timeRemaining <= 0) {
     endGame();
+    //Include showing the end screen
   } else {
     clockTimer.innerHTML = timeRemaining;
     timeRemaining--;
@@ -20,7 +21,9 @@ var startBtn = document.getElementById("start");
 //Answering the questions
 var questionsDiv = document.getElementById("questions");
 var answerChoices = document.getElementsByClassName("choice");
+var currentQuestion = questionsArray[questionIndex];
 var questionIndex = 0;
+var correctAnswers = ["Klingon", "()", "loop"];
 
 //Submitting initials and scores
 var initials = document.getElementById("initials");
@@ -46,25 +49,53 @@ var showQuestions = function () {
   questPrompt.textContent = currentQuestion.questionText;
   for (let i = 0; i < answerChoices.length; i++) {
     answerChoices[i].innerText = currentQuestion.choices[i];
-  }
 
-  // event listener for each button (maybe in the above for loop)
-  // check if the text for the current chose answer matches the one in your currentQuestion
-  // if yes, move onto following question else subtract time
+    //Adding an event listener for each for each button
+    //They seem to be triggered by any button, including submit, and also it is applying to all 4
+    //Have another look at this - https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_element_addeventlistener2
+    var choiceBtn1 = document.getElementById("choice1");
+    choiceBtn1.addEventListener("click", handleAnswer);
+    console.log("clicked on choice 1");
+    var choiceBtn2 = document.getElementById("choice2");
+    choiceBtn2.addEventListener("click", handleAnswer);
+    var choiceBtn3 = document.getElementById("choice3");
+    choiceBtn3.addEventListener("click", handleAnswer);
+    var choiceBtn4 = document.getElementById("choice4");
+    choiceBtn4.addEventListener("click", handleAnswer);
+  }
 };
-// What happens with the answer - if text matches the one in
+
+//currentQuestion.correctAnswer?
+
 var handleAnswer = function () {
-  if (this.value !== currentQuestion.correctAnswer) {
+  console.log("Are they being moved here");
+  // Check that the text for the answer matches the answer in the currentQuestion
+  //If the answer is wrong, subtract 10 from the time
+  //Do I need to create a variable for user answer?
+  if (this.value !== correctAnswers) {
     timeRemaining -= 10;
-    // if it's correct, move to the next question
-    // if it's incorrect, alert to say that the answer is wrong
-    // if time has run out, the game ends
-    if (timeRemaining <= 0) {
-      endGame();
-    }
+    //If the answer is right, show the next question
+  } else {
+    showQuestions();
+    questionIndex++;
   }
 };
 
+//Give user feedback on their answer - correct/incorrect
+//var answerFeedback = getElementById("feedback");
+//answerFeedback.textContent = "Sorry, that's wrong!";
+//answerFeedback.textContent = "Nice work!";
+
+//Dealing with the clock timer. Need moving up?
+//if (timeRemaining < 0) {
+//timeRemaining = 0;
+//clockTimer.textContent = timeRemaining;
+//Once run out of questions, end game
+//} else if (questionIndex === questionsArray.length) {
+//endGame();
+//}
+
+//Need to show end screen
 var endGame = function () {
   clearInterval(timerId);
 };
