4078dff95014854faa8ed738c4a0233da68d3f62
Charlotte Brazier <charlottebrazier@hotmail.com>
2023-08-03 11:46:11 +0100

    started adding 60-second countdown timer

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,8 +1,18 @@
 // List variables and add document query selectors
 
 // Time-related
-var startingTime = 60;
+var timeRemaining = 60;
 var clockTimer = document.getElementById("timer");
+var timerId = setInterval(countdown, 1000);
+function countdown() {
+  if (timeRemaining == 0) {
+    clearInterval(timerId);
+    //Startaskingquestions();
+  } else {
+    clockTimer.innerHTML = timeRemaining;
+    timeRemaining--;
+  }
+}
 
 //Starting the quiz
 var startDiv = document.getElementById("start-screen");
@@ -20,11 +30,18 @@ var seeHighScores = document.getElementById("scores");
 
 // Player starts game by clicking start button
 var startGame = function () {
-  clockTimer.innerText = startingTime;
+  clockTimer.innerText = timeRemaining;
   startDiv.classList.add("hide"); // adds the "hide" class
-  questionsDiv.classList.remove("hide"); // reomves the "hide" class
+  questionsDiv.classList.remove("hide"); // removes the "hide" class
   showQuestions();
 };
+
+// What is happening to the countdown clock
+setInterval(function () {
+  if (clockTimer > 0) clockTimer--;
+  document.getElementById("timer").innerHTML = timeRemaining;
+  if (clockTimer <= 0) clockTimer = 60;
+}, 1000);
 
 // Questions are displayed
 var showQuestions = function () {
@@ -35,7 +52,7 @@ var showQuestions = function () {
     answerChoices[i].innerText = currentQuestion.choices[i];
   }
 
-  // event listener for each button (maybe in the baove for loop)
+  // event listener for each button (maybe in the above for loop)
   // check if the text for the current chose answer matches the one in your currentQuestion
   // if yes, move onto following question else subtract time
 };
