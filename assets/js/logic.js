ef2671145377c743df4e679f5aa038a0abe38d6b
Charlotte Brazier <charlottebrazier@hotmail.com>
2023-08-05 21:05:43 +0100

    added logic to get from one question to the next, and the inner HTML feedback to correct or incorrect answers to appear on screen

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -13,7 +13,6 @@ function countdown() {
     timeRemaining--;
   }
 }
-
 //Starting the quiz
 var startDiv = document.getElementById("start-screen");
 var startBtn = document.getElementById("start");
@@ -21,9 +20,13 @@ var startBtn = document.getElementById("start");
 //Answering the questions
 var questionsDiv = document.getElementById("questions");
 var answerChoices = document.getElementsByClassName("choice");
+var questionDisplayed = document.getElementById("question-title");
 var currentQuestion = questionsArray[questionIndex];
 var questionIndex = 0;
-var correctAnswers = ["Klingon", "()", "loop"];
+var questionPosition = -1;
+
+//Scores
+var currentScore = 0;
 
 //Submitting initials and scores
 var initials = document.getElementById("initials");
@@ -36,7 +39,7 @@ var finalScore = document.getElementById("final-score");
 
 // Player starts game by clicking start button
 var startGame = function () {
-  clockTimer.innerText = timeRemaining;
+  countdown();
   startDiv.classList.add("hide"); // adds the "hide" class
   questionsDiv.classList.remove("hide"); // removes the "hide" class
   showQuestions();
@@ -51,11 +54,8 @@ var showQuestions = function () {
     answerChoices[i].innerText = currentQuestion.choices[i];
 
     //Adding an event listener for each for each button
-    //They seem to be triggered by any button, including submit, and also it is applying to all 4
-    //Have another look at this - https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_element_addeventlistener2
     var choiceBtn1 = document.getElementById("choice1");
     choiceBtn1.addEventListener("click", handleAnswer);
-    console.log("clicked on choice 1");
     var choiceBtn2 = document.getElementById("choice2");
     choiceBtn2.addEventListener("click", handleAnswer);
     var choiceBtn3 = document.getElementById("choice3");
@@ -65,21 +65,43 @@ var showQuestions = function () {
   }
 };
 
-//currentQuestion.correctAnswer?
-
-var handleAnswer = function () {
+// Check that the text in the button matches the text in the correct answer.
+// If they get it right, move to the next question, if it's wrong, minus 10 off the time
+function handleAnswer(event) {
   console.log("Are they being moved here");
-  // Check that the text for the answer matches the answer in the currentQuestion
-  //If the answer is wrong, subtract 10 from the time
-  //Do I need to create a variable for user answer?
-  if (this.value !== correctAnswers) {
-    timeRemaining -= 10;
-    //If the answer is right, show the next question
+  var correctAnswers = "Klingon" || "()" || "loop";
+  if (event.target.textContent === correctAnswers) {
+    feedback.innerHTML += "Correct! You definitely know your stuff!";
+    nextQuestion();
   } else {
-    showQuestions();
-    questionIndex++;
+    timeRemaining -= 10;
+    feedback.innerHTML +=
+      "Close, but that is incorrect. Keep going - you've got this!";
+    nextQuestion();
   }
-};
+}
+
+function nextQuestion() {
+  console.log("Clicked on the right answer");
+  questionPosition++;
+  if (questionPosition === questionsArray.length) {
+    timeRemaining = 0;
+    endGame();
+  } else {
+    questionIndex++;
+    showQuestions();
+  }
+}
+
+// Check that the text for the answer matches the answer in the currentQuestion
+//If the answer is wrong, subtract 10 from the time
+//Do I need to create a variable for user answer?
+//if (this.value !== correctAnswers) {
+//If the answer is right, show the next question
+//} else {
+//showQuestions();
+//questionIndex++;
+//}
 
 //Give user feedback on their answer - correct/incorrect
 //var answerFeedback = getElementById("feedback");
@@ -98,6 +120,7 @@ var handleAnswer = function () {
 //Need to show end screen
 var endGame = function () {
   clearInterval(timerId);
+  prompt("Time is up");
 };
 
 // event listeners
